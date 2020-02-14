e69f0e8733b625e3e0dc04436fd2d5f13374b4d7
Amit <>
2020-02-14 18:33:56 +0530

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,20 +2,14 @@ import { TestBed, async, ComponentFixture, inject } from '@angular/core/testing'
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CommonService } from './services/common.service';
-import { Employee } from './models/Employee.model';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Users } from './mockdata/Users';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
   const formBuilder: FormBuilder = new FormBuilder();
-  interface Employee {
-      name: string;
-      empcode: string;
-      department: string;
-      grade: string;
-  }
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -27,12 +21,13 @@ describe('AppComponent', () => {
         AppComponent
       ],
       providers: [CommonService]
-    }).compileComponents().then(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      component = fixture.debugElement.componentInstance;
-      // service = TestBed.get(CommonService);
-    });
+    }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+  });
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
@@ -53,12 +48,7 @@ describe('AppComponent', () => {
 
   it('should validate form after entering values in the form', () => {
     expect(component.submitted).toBe(false);
-    component.registerForm = formBuilder.group({
-      name: 'amitddd',
-      empcode: 10101,
-      department: 'os',
-      grade: 'L4'
-    });
+    component.registerForm = formBuilder.group(Users[0]);
     component.onSubmit();
     expect(component.submitted).toBe(true);
   });
